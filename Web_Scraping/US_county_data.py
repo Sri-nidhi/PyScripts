48139c80fc0f4f758ba740e0b3d1387d2f41adb5
Sri-Nidhi <srinidhi.s96@gmail.com>
2020-05-27 15:00:57 +0530

    Update US_county_data.py

--- Web_Scraping/US_county_data.py
+++ Web_Scraping/US_county_data.py
@@ -4,11 +4,13 @@ import json
 from lxml import html
 import pandas as pd
 import csv
+final_df = []
 
-for i in county['Georgia']:
+for i in county_list:
     url = "http://www.statsamerica.org/USCP/geo_response_HC.aspx?term={place}".format(place = i)
     response = requests.post(url, data=payload)
     county_ids.append(json.loads(response.text)[0]['id'])
+    print(county_ids)
 county_string = '%5E'.join(county_ids)
 
  
@@ -26,5 +28,9 @@ table_header = [cell.text for cell in BeautifulSoup(html_data)("th")]
 table_data = [[cell.text for cell in row("td")]
                          for row in BeautifulSoup(html_data)("tr")]
 df = pd.DataFrame(table_data[1:], columns = table_header) 
-
-df.to_excel('Counties.xlsx')
+df1 = df.transpose()
+df1.reset_index(drop=False, inplace=True)
+new_header = df1.iloc[0] #grab the first row for the header
+df1 = df1[1:] #take the data less the header row
+df1.columns = new_header
+df1
